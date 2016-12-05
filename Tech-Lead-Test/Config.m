15bb79a9cd93fdfdf587a80ee6cbea9f8cd2ee9c
Kyle <kyle.langille@eventbase.com>
2016-12-05 10:56:33 -0800

    Initial Commit

---
+++ Tech-Lead-Test/Config.m
@@ -0,0 +1,15 @@
+//
+//  Config.m
+//  Tech-Lead-Test
+//
+//  Created by Kyle on 2016-12-02.
+//  Copyright © 2016 Eventbase. All rights reserved.
+//
+
+#import "Config.h"
+
+NSString* const serverAuthUrl = @"http://localhost.charlesproxy.com:9999/techleadtest-invalid/auth.php";
+NSString* const serverDataUrl = @"http://localhost.charlesproxy.com:9999/techleadtest-invalid/data.php";
+
+NSString* const serverToken = @"12345679";
+
